1c41ea2aba270cdfaa58cc2158f42ed530cd498d
majjoha <mathias@mjj.io>
2014-02-01 17:30:33 +0100

    Support ERB as well.

--- lib/dolphy/templates.rb
+++ lib/dolphy/templates.rb
@@ -1,10 +1,26 @@
+require 'haml'
+require 'erb'
+
+# This module provides a selection of template rendering methods.
+#
+# They are used like in Sinatra in the following way:
+#
+# get '/hello' do
+#   haml :index, :body => "Hello"
+# end
+# 
 module Dolphy
-  # This class provides a selection of template rendering methods.
   module Templates
     def haml(template_name, locals)
       template = File.open("views/#{template_name.to_s}.haml").read
       engine = Haml::Engine.new(template)
       engine.render(Object.new, locals)
+    end
+
+    def erb(template_name, locals)
+      template = File.open("views/#{template_name.to_s}.erb").read
+      engine = ERB.new(template)
+      engine.result(OpenStruct.new(locals).instance_eval { binding })
     end
   end
 end
