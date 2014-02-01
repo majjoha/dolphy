883efe32e4a7f8f7263415e99dbe39926c7b3d0f
majjoha <mathias@mjj.io>
2014-02-01 16:54:27 +0100

    Move HTTP methods to router.

--- lib/dolphy/router.rb
+++ lib/dolphy/router.rb
@@ -1,4 +1,19 @@
 module Dolphy
-  class Router
+  module Router
+    def get(path, &block)
+      add_route(:get, path, block)
+    end
+
+    def post(path, &block)
+      add_route(:post, path, block)
+    end
+
+    def put(path, &block)
+      add_route(:put, path, block)
+    end
+
+    def delete(path, &block)
+      add_route(:delete, path, block)
+    end
   end
 end
