bdbebebbe0fd05bd93d2d26b9f3ad8a685859675
majjoha <mathias@mjj.io>
2014-02-02 17:53:03 +0100

    Test #post method as well.

--- spec/dolphy_spec.rb
+++ spec/dolphy_spec.rb
@@ -4,9 +4,18 @@ describe 'DolphyApplication' do
   include Rack::Test::Methods
 
   describe '#get' do
-    it 'should return "Hello" on /hello' do
-      visit '/hello'
+    it 'returns a page saying Hello on a correct path' do
+      visit '/'
       expect(page).to have_content "Hello"
+    end
+  end
+
+  describe '#post' do
+    it 'returns a page saying Hello Mathias' do
+      visit '/'
+      fill_in('message[name]', with: 'Mathias')
+      click_button('Submit')
+      expect(page).to have_content "Hello Mathias"
     end
   end
 end
