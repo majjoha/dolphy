3deb46d2f9a1e09845863e8d6b924acbcb076403
majjoha <mathias@mjj.io>
2014-02-09 15:14:11 +0100

    Fix path to views.

--- lib/dolphy/template_engines.rb
+++ lib/dolphy/template_engines.rb
@@ -12,13 +12,13 @@ require 'erb'
 module Dolphy
   module TemplateEngines
     def haml(template_name, locals)
-      template = File.open("example/views/#{template_name.to_s}.haml").read
+      template = File.open("views/#{template_name.to_s}.haml").read
       engine = Haml::Engine.new(template)
       engine.render(Object.new, locals)
     end
 
     def erb(template_name, locals=nil)
-      template = File.open("example/views/#{template_name.to_s}.erb").read
+      template = File.open("views/#{template_name.to_s}.erb").read
       engine = ERB.new(template)
       engine.result(OpenStruct.new(locals).instance_eval { binding })
     end
