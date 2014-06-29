7cfb5652884326d14605c765ea7b93aa00e652e6
majjoha <mathias@mjj.io>
2014-06-29 17:24:02 +0200

    Add a simple wrapper class for requests.

---
+++ lib/dolphy/request.rb
@@ -0,0 +1,22 @@
+require 'rack'
+
+module Dolphy
+  # This acts as a simple wrapper class for a request. Its main purpose is to
+  # simplify how the framework receives information about the path and HTTP
+  # method of a request.
+  class Request < Rack::Request
+    attr_accessor :env
+
+    def initialize(env)
+      @env = env
+    end
+
+    def http_method
+      @env['REQUEST_METHOD'].downcase.to_sym
+    end
+
+    def path
+      @env['PATH_INFO']
+    end
+  end
+end
