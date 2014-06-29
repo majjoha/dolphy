29302a8aa2a03c84bdb30faeaf15f7d9b11b0146
majjoha <mathias@mjj.io>
2014-06-29 17:23:10 +0200

    Simplify router and make it a class instead of a module.

--- lib/dolphy/router.rb
+++ lib/dolphy/router.rb
@@ -1,27 +1,15 @@
-# This module handles all the methods that adds routes to our route array.
-# It has an array containing all the routes ordered by HTTP method.
 module Dolphy
-  module Router
+  class Router
     attr_accessor :routes
 
-    def initialize_router
+    def initialize
       @routes = { :get => {}, :post => {}, :put => {}, :delete => {} }
-    end
-
-    def not_found
-      erb :not_found
     end
 
     %w(get post put delete).each do |verb|
       define_method(verb) do |path, &block|
-        add_route(verb.to_sym, path, block)
+        routes[verb.to_sym][path] = block
       end
-    end
-
-    private
-
-    def add_route(http_method, path, block)
-      routes[http_method][path] = block
     end
   end
 end
