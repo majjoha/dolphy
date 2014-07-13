ca37c4a9e20ce7955c53de349d58780a449803ce
majjoha <mathias@mjj.io>
2014-07-12 20:17:04 -0700

    Simplify template engine.

---
+++ lib/dolphy/template_engine.rb
@@ -0,0 +1,36 @@
+require 'tilt'
+
+module Dolphy
+  module TemplateEngines
+    def erb(template_name, locals)
+      template = Tilt::ERBTemplate.new("views/#{template_name.to_s}.erb")
+      template.render(Object.new, locals)
+    end
+
+    def haml(template_name, locals)
+      template = Tilt::HamlTemplate.new("views/#{template_name.to_s}.haml")
+      template.render(Object.new, locals)
+    end
+  end
+
+  class TemplateEngine
+    include Dolphy::TemplateEngines
+
+    def initialize(template_engine)
+      @template_engine = template_engine
+    end
+
+    def render(template_name, locals)
+      case template_engine
+      when :erb
+        erb(template_name, locals)
+      when :haml
+        haml(template_name, locals)
+      end
+    end
+
+    private
+
+    attr_accessor :template_engine
+  end
+end
