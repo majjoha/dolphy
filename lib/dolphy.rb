36c23b9b4e6611f1b64d86eb9921ac134fbd0998
majjoha <mathias@mjj.io>
2014-07-12 20:26:19 -0700

    Remove app example from code.

--- lib/dolphy.rb
+++ lib/dolphy.rb
@@ -2,18 +2,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'dolphy/core'
 
 class DolphyApp
-# This returns a DolphyApplication defined by what is passed in the block.
-#
-# An application could for instance be defined in this way
-#
-# DolphyApp.app do
-#   DolphyApp.router do
-#     get '/hello' do
-#       haml :index, body: "Hello"
-#     end
-#   end
-# end.serve!
-#
   def self.app(&block)
     Dolphy::Core.new(&block)
   end
