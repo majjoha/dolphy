6af14565d97e63d3f54bed6815c371fa46fab974
majjoha <mathias@mjj.io>
2014-07-19 09:25:11 -0700

    Add spec for #[]=.

--- spec/dolphy/settings_spec.rb
+++ spec/dolphy/settings_spec.rb
@@ -3,10 +3,6 @@ require_relative '../spec_helper'
 describe Dolphy::Settings do
   let(:settings) { Dolphy::Settings.new }
 
-  it "is an instance of Dolphy::Settings" do
-    expect(settings).to be_a(Dolphy::Settings)
-  end
-
   describe "#initialize" do
     it "initializes an empty configuration hash" do
       expect(settings.settings).to eq(
@@ -16,11 +12,22 @@ describe Dolphy::Settings do
         }
       )
     end
+
+    it "is an instance of Dolphy::Settings" do
+      expect(settings).to be_a(Dolphy::Settings)
+    end
   end
 
   describe "#[]" do
     it "finds the selected element from the settings hash" do
       expect(settings[:template_engine]).to eq :erb
+    end
+  end
+
+  describe "#[]=" do
+    it "sets the key value pair in the settings hash properly" do
+      settings[:checkout] = 12
+      expect(settings[:checkout]).to be 12
     end
   end
 end
